a9a9a23bdf4bd1cf8b11281b0e396f1fb5179a17
Julianna Calabrese <juliannarosecalabrese@gmail.com>
2021-05-18 19:20:39 -0400

    Better examples!

--- R/colors.R
+++ R/colors.R
@@ -13,19 +13,19 @@ joycon_palettes <- list(
   yoshi = c("#6fe46b", "#fe8c50", "#fb4933"),
   littlemac = c("#686a67", "#90e26f", "#1f6720"),
   # Palettes with five colors each
-  supermario = c("#E4000F", "#049CD8", "#FBD000", "#43B047", "#000000"),
-  skywardsword = c("#506538", "#D4AF37", "#7bdfd0", "#67c0fc", "#6b5ab6"),
+  supermario = c("#e4000f", "#049cd8", "#fbd000", "#43b047", "#000000"),
+  skywardsword = c("#506538", "#d4af37", "#7bdfd0", "#67c0fc", "#6b5ab6"),
   starbits = c("#ffe668", "#b9ff84", "#e560f9", "#7890d8", "#e36f46"),
-  newhorizons = c("#c47636", "#f1bf2c", "#3fc3d8", "#02a47b", "#eca759"),
+  newhorizons = c("#c47636", "#eca759", "#f1bf2c", "#3fc3d8", "#02a47b"),
   rainbowroad = c("#ff1903", "#ff1eed", "#1e24ef", "#12e2ec", "#1afc07"),
-  splatoon = c("#fa3296", "#aadc00", "#f75900", "#00c8b4", "#c800dc"),
+  splatoon = c("#fa3296", "#f75900", "#aadc00", "#00c8b4", "#c800dc"),
   isabelle = c("#ffaa55", "#ab4a3b", "#a2b0ac", "#808000", "#ffff80"),
-  kirby = c("#ea86ab", "#17193e", "#9c162b", "#d12c54", "#f9d2ef"),
-  wiisports = c("#88B63A", "#45a7c7", "#a48e60", "#ececef", "#37743a"),
-  twilightprincess = c("#846048", "#3e877a", "#c098b6", "#698454", "#783a77"),
+  wiisports = c("#37743a", "#88B63A", "#45a7c7", "#a48e60", "#ececef"),
+  twilightprincess = c("#846048", "#3e877a", "#698454", "#c098b6", "#783a77"),
   delfino = c("#00d5d3", "#0080de", "#fdbc67", "#fbe9b9", "#da6a44"),
   samus = c("#77d9e2", "#fdb043", "#a73941", "#419a8e", "#c4ee89"),
-  duckhunt = c("#82d30c", "#c94b04", "#3dc0ff", "#005909", "#f97661"),
+  duckhunt = c("#c94b04", "#f97661", "#3dc0ff", "#82d30c", "#005909"),
+  princesspeach = c("#ffb3e2", "#fe89a8", "#f15980", "#f8e31f", "#54c5f8"),
   # Palettes with eight colors each
   korok = c("#5d5536", "#d1ac9e", "#a3ac89", "#958879", "#925f4e", "#5b211e", "#78875c", "#9f9fa0"),
   majora = c("#59a91d", "#724ace", "#c401d1", "#007879", "#800000", "#ffcc33", "#0098ef", "#ff5f51"),
@@ -37,7 +37,7 @@ joycon_palettes <- list(
 #'
 #'@param name Name of color palette.
 #'Palettes include: \code{supermario}, \code{skywardsword}, \code{starbits},
-#'\code{newhorizons}, \code{rainbowroad}, \code{splatoon},
+#'\code{newhorizons}, \code{rainbowroad}, \code{splatoon},  \code{princesspeach},
 #'\code{yoshi}, \code{isabelle}, \code{kirby}, \code{twilightprincess}, \code{wiisports},
 #'\code{korok}, \code{majora}, \code{duckhunt}, \code{littlemac},
 #'\code{samus}, \code{delfino}, and \code{hyrule}.
