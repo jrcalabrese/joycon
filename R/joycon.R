ccd38cc236cd4b4d41f4f55828ffa7d50ff70b24
Julianna Calabrese <juliannarosecalabrese@gmail.com>
2021-05-19 11:10:06 -0400

    Added package description

---
+++ R/joycon.R
@@ -0,0 +1,8 @@
+#' @title joycon
+#' @name joycon
+#' @description The `joycon` package provides several color palettes inspired by Nintendo games,
+#' including Super Mario, Animal Crossing, and the Legend of Zelda.
+#' @author Julianna Calabrese
+#' @docType package
+#' @keywords color palette nintendo
+NULL
