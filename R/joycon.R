52da23849e91e0a7a881fe32a6fa571d2e59f0a3
Julianna Calabrese <juliannarosecalabrese@gmail.com>
2021-05-19 11:45:59 -0400

    Moved the logo just a bit

--- R/joycon.R
+++ R/joycon.R
@@ -1,12 +1,10 @@
 #' @title joycon
 #'
-#' \if{html}{\figure{hexsticker.PNG}{options: width="20%"}}
-#'
-#' @md
-#'
 #' @name joycon
 #' @description The `joycon` package provides several color palettes inspired by Nintendo games,
 #' including Super Mario, Animal Crossing, and the Legend of Zelda.
+#' \if{html}{\figure{hexsticker.PNG}{options: width="20%"}}
+#' @md
 #' @author Julianna Calabrese
 #' @docType package
 #' @keywords color palette nintendo
