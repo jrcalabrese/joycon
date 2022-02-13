01ce520c493f3af5b2ffe9057b32d8a41c625fef
Julianna Calabrese <juliannarosecalabrese@gmail.com>
2022-02-12 19:22:15 -0500

    Update joycon_list.R

--- R/joycon_list.R
+++ R/joycon_list.R
@@ -2,7 +2,7 @@
 #'
 #' @description Palettes are available in three, five, and eight colors.
 #'
-#' Use `joycon_list` to see all palettes available.
+#' Use `joycon_list` to see all palettes available. Can add additional palettes upon request.
 #'
 #' @export
 joycon_list <- function(){
